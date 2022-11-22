7e244ee27889907b706d211fc2473dbb87d9ee65
thpvrs <63070104@kmitl.ac.th>
2022-11-22 17:01:18 +0700

    Initial commit

---
+++ src/test/java/com/sop/chapter13/bookstoreservice/BookstoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.chapter13.bookstoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
